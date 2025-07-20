686749ae9462947625b1b871030d743ce011221a
Acr1d3rm <ivan8Z7654321X@mail.ru>
2025-07-20 17:34:47 +0300

    Добавьте файлы проекта.

---
+++ MathGame/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            var pairsOfEmoji = new List<string>
+            {
+                "❤","❤",
+                "🎂","🎂",
+                "😜","😜",
+                "👀", "👀",
+                "🤘","🤘",
+                "👂","👂",
+                "👎","👎",
+                "👍","👍",
+            };
+
+            var random = new Random();
+
+            foreach (TextBlock textBlock in MainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(pairsOfEmoji.Count);
+                string nextEmoji = pairsOfEmoji[index];
+
+                textBlock.Text = nextEmoji;
+                pairsOfEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
